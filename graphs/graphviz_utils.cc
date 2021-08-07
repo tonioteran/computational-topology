6fa9f3cb4525a6a646e07fc10b645762b385edbd
tonio teran <teran@mit.edu>
2021-08-07 10:26:45 -0700

    Add function to draw Tutte embedding.

--- graphs/graphviz_utils.cc
+++ graphs/graphviz_utils.cc
@@ -1,6 +1,11 @@
-#include "graphviz_utils.h"
+// Simple utilities for producing dot graphviz files from custom graphs.
+// Copyright 2021 Antonio Teran (toniioteran@gmail.com)
+#include "graphs/graphviz_utils.h"
 
 #include <fstream>
+#include <string>
+
+#include "graphs/tutte_embedding.h"
 
 namespace topo {
 
@@ -15,6 +20,27 @@ void WriteGraphToDisk(const std::string& output_file,
     } else {
       out_stream << v.parent->index << " -> " << v.index << "\n";
     }
+  }
+
+  out_stream << "}\n";
+  out_stream.close();
+}
+
+void DrawStraightLineEmbedding(const std::string& output_file,
+                               const PlanarGraph& G, double scale) {
+  const Eigen::MatrixXd positions = FindTutteEmbedding(G);
+  std::ofstream out_stream(output_file);
+  out_stream << "graph {\n";
+
+  // Set the individual nodes in their own embedding positions.
+  for (size_t v = 0; v < G.N(); ++v) {
+    const Eigen::Vector2d p = scale * positions.col(v);
+    out_stream << v << " [pos=\"" << p(0) << "," << p(1) << "!\"]\n";
+  }
+
+  // Draw the straight lines for each edge.
+  for (const PlanarGraph::Edge& e : G.Edges()) {
+    out_stream << e.first << " -- " << e.second << "\n";
   }
 
   out_stream << "}\n";
--- graphs/graphviz_utils.h
+++ graphs/graphviz_utils.h
@@ -1,16 +1,23 @@
-// Simple utilities for producing dot graphviz files from disjoint set systems.
-// Antonio Teran (toniioteran@gmail.com)
+// Simple utilities for producing dot graphviz files from custom graphs.
+// Copyright 2021 Antonio Teran (toniioteran@gmail.com)
 
 #ifndef GRAPHS_GRAPHVIZ_UTILS_H_
 #define GRAPHS_GRAPHVIZ_UTILS_H_
 
-#include "disjoint_set_system.h"
+#include <string>
+
+#include "graphs/disjoint_set_system.h"
+#include "graphs/planar_graph.h"
 
 namespace topo {
 
 // Creates graphviz-compatible dot file with the `system`s graph representation.
 void WriteGraphToDisk(const std::string& output_file,
                       const DisjointSetSystem& system);
+
+// Draws straight-line embedding for a planar graph as a graphviz graph to disk.
+void DrawStraightLineEmbedding(const std::string& output_file,
+                               const PlanarGraph& G, double scale = 1.0);
 
 }  // namespace topo
 
